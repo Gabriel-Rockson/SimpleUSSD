861cced6dd06b608838f9446f24f8d494b436dac
Gabriel Rockson <gabriel.rockson@amalitech.com>
2023-06-16 09:24:05 +0000

    Add static methods for displaying options to user

---
+++ src/main/java/ussd/Prompt.java
@@ -0,0 +1,41 @@
+package ussd;
+
+public abstract class Prompt {
+
+    private Prompt() {}
+
+    public static void showOptions(String[] options) {
+        for (int i = 0; i < options.length; i++) {
+            System.out.println(i + 1 + " - " + options[i]);
+        }
+    }
+
+    public static void showOptions(String[] options, boolean withGoBack) {
+        showOptions(options);
+        if (withGoBack) {
+            System.out.println("00 - go back");
+        }
+    }
+
+    public static void showOptions(String promptMessage, String[] options) {
+        System.out.println(promptMessage);
+        showOptions(options);
+    }
+
+    public static void showOptions(String promptMessage, String[] options, boolean withGoBack) {
+        System.out.println(promptMessage);
+        showOptions(options, withGoBack);
+    }
+
+    public static void invalidInput() {
+        System.err.println("Invalid entry, quitting application ...\n");
+        System.exit(0);
+    }
+
+    public static void endOfApplication() {
+        System.out.println("Feature coming soon");
+        System.out.println("End of application process...\n");
+        System.exit(1);
+    }
+
+}
